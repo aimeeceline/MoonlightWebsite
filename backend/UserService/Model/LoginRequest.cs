c5d187c3255fb0862a06114072820a182f37a22f
aimeeceline <lelananh02@gmail.com>
2025-12-17 16:58:27 +0700

    update

--- backend/UserService/Model/LoginRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿// LoginRequest.cs
-namespace UserService.Model
-{
-    public class LoginRequest
-    {
-        public string Username { get; set; } = string.Empty; // 👈 thêm mặc định
-        public string Password { get; set; } = string.Empty; // 👈 thêm mặc định
-    }
-}
